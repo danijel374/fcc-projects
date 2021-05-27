04467dbda684ce67dad46321b2a13d6d1d130db3
Prskalo <danijelprskalo7@gmail.com>
2021-05-27 22:06:16 +0200

    added random quote react project initial commit still needs some work

---
+++ front-end-development/random-quote-react/src/App.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import classes from './App.module.scss';
+import { FaTwitter, FaQuoteLeft } from 'react-icons/fa';
+
+// create layout
+// fetch quotes
+// populate quote
+// select random quote and update state
+
+function App() {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getRandomQuote = (quotes) => {
+    const randomNum = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomNum];
+    setIsLoading(false);
+    return randomQuote;
+  };
+
+  const sendRequest = () => {
+    fetch(
+      'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const randomQuote = getRandomQuote(data.quotes);
+        setQuote(randomQuote.quote);
+        setAuthor(randomQuote.author);
+      });
+  };
+
+  useEffect(() => {
+    sendRequest();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const content = isLoading ? (
+    <div id={classes.loader}></div>
+  ) : (
+    <header>
+      <FaQuoteLeft />
+      <cite id='text'>{quote}</cite>
+      <p id='author'> - {author}</p>
+    </header>
+  );
+
+  return (
+    <div className={classes.App} id='quote-box'>
+      {content}
+      <div>
+        <button>
+          <a
+            id='tweet-quote'
+            target='_blank'
+            rel='noreferrer'
+            href={`https://twitter.com/intent/tweet?text=${quote} - ${author}`}
+          >
+            <FaTwitter />
+          </a>
+        </button>
+        <button id='new-quote' onClick={sendRequest}>
+          New Quote
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
